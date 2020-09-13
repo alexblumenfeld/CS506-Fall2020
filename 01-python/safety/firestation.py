345650b6bb5e1975dc735f2104d6c63befe5c2cb
Alex Blumenfeld <alexblumenfeld.ab@gmail.com>
2020-09-12 20:38:00 -0400

    Adding fire station with variable number of trucks

--- 01-python/safety/firestation.py
+++ 01-python/safety/firestation.py
@@ -1,3 +1,14 @@
-def draw_firestation():
-    print("fire station not found")
+def draw_firestation(trucks=1):
+    print("________________")
+    print("|_|_||_||_||_|_|")
+    print("|  |  |  |  |  |")
+    print("|  F  I  R  E  |")
+    print("|  D  E  P  T  |")
+    print("|  __________  |")
+    print("|  |        |  |" + "  ==================" * trucks)
+    print("|  | []  [] |  |" + "  _|_|___|__|__|_   " * trucks)
+    print("|  |        |  |" + "  |              |-|" * trucks)  
+    print("|  | []  [] |  |" + "  |________________|" * trucks)
+    print("|  |        |  |" + "    []         []   " * trucks)
+    print("----------------" + "--------------------" * trucks)
     return
