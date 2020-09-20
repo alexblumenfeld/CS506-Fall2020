d52b87da27f5fa8a750bf5ab50825b63e5e27adb
Alex Blumenfeld <alexblumenfeld.ab@gmail.com>
2020-09-20 16:28:19 -0400

    Implemented CSV reader and distance functions

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,19 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    csv_file = open(csv_file_path, 'r')
+    lines = csv_file.readlines()
+    for i in range(len(lines)):
+        sub_list = lines[i].split(',')
+        for j in range(len(sub_list)):
+            # Deal with extra sets of quotation marks around strings
+            if '"' in sub_list[j]:
+                sub_list[j] = sub_list[j][1:-1]
+            # Deal with the newline characters in the CSV
+            if '\n' in sub_list[j]:
+                sub_list[j] = sub_list[j][:-1]
+            # Deal with numbers that are currently expressed as strings
+            if sub_list[j].isnumeric():
+                sub_list[j] = int(sub_list[j])
+        lines[i] = sub_list
+    return lines
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,47 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        raise ValueError('lengths must be equal')
+    elif x == [] or y == []:
+        raise ValueError('lengths must not be zero')
+    distance_components = [0 for num in range(len(x))]
+    for i in range(len(x)):
+        distance_components[i] = (x[i] - y[i])**2
+    return sum(distance_components)**0.5
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        raise ValueError('lengths must be equal')
+    elif x == [] or y == []:
+        raise ValueError('lengths must not be zero')
+    distance_components = [0 for num in range(len(x))]
+    for i in range(len(x)):
+        distance_components[i] = abs(x[i] - y[i])
+    return sum(distance_components)
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        raise ValueError('lengths must be equal')
+    elif x == [] or y == []:
+        raise ValueError('lengths must not be zero')
+    union = len(x)
+    intersection = 0
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            intersection += 1
+    return 1 - intersection / union
+
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y):
+        raise ValueError('lengths must be equal')
+    elif x == [] or y == []:
+        raise ValueError('lengths must not be zero')
+    x_length = sum([num**2 for num in x])**0.5
+    y_length = sum([num**2 for num in y])**0.5
+    if x_length == 0 or y_length == 0:
+        raise ValueError('lengths must not be zero')
+    inner_product = sum([x[i] * y[i] for i in range(len(x))])
+    cosine_val = inner_product / (x_length * y_length)
+    return cosine_val
 
 # Feel free to add more
